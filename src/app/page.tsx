75648b90b9535a2dfe1f48569e25cbf399a12019
Silvi Sinanaj <silvi.sinanaj@blacklane.com>
2023-08-31 15:42:16 +0200

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Explore Berat is coming soon . . .</h1>
+      <h2>Explore Berat is coming soon . . .</h2>
       <video autoPlay loop muted>
         <source src='/loadingplane.webm' type='video/webm' />
       </video>
