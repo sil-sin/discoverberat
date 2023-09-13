ba5df8979844c92e4943b3bc47741405ae080361
Silvi Sinanaj <silvi.sinanaj@blacklane.com>
2023-09-13 22:52:33 +0200

    added icon library, added mobile menu , added props to button

--- src/components/sectors/Navigation/Navigation.tsx
+++ src/components/sectors/Navigation/Navigation.tsx
@@ -1,19 +1,36 @@
 'use client'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import Image from 'next/image'
 import { HEADER_LINKS } from './utils'
 import styles from './Navigation.module.css'
 import Button from '@/components/simple/Button'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import classNames from 'classnames'
 
 type Props = {}
 export const Navigation: FC<{}> = () => {
+  const [isMenuShow, setIsMenuShow] = useState(false)
+
   return (
     <nav className={styles.navbarContainer}>
       <a rel='noopener' className={styles.logo} href='/'>
         <Image src='/vercel.svg' width={100} height={70} alt='Explore Berat' />
       </a>
+      <Button
+        className={styles.menuButton}
+        variant='secondary'
+        onClick={() => {
+          setIsMenuShow(!isMenuShow)
+        }}
+      >
+        {isMenuShow ? <AiOutlineClose /> : <AiOutlineMenu />}
+      </Button>
 
-      <ul className={styles.navbarLinksContainer}>
+      <ul
+        className={classNames(
+          isMenuShow ? styles.navbarLinksShow : styles.navbarLinksContainer
+        )}
+      >
         {HEADER_LINKS.map((link: any) => (
           <li key={link.title}>
             <Button variant='link' href={link.href} text={link.title} />
--- src/components/simple/Button/Button.tsx
+++ src/components/simple/Button/Button.tsx
@@ -1,24 +1,38 @@
-import { FC } from 'react'
+'use client'
+import { FC, ReactNode } from 'react'
 import styles from './Button.module.css'
 import classnames from 'classnames'
 
 type Props = {
   onClick?: () => void
-  text: string
+  text?: string
   variant?: 'primary' | 'secondary' | 'tertiary' | 'link'
   href?: string
+  children?: ReactNode
+  className?: string
 }
-export const Button: FC<Props> = ({ onClick, text, variant, href }) => {
+export const Button: FC<Props> = ({
+  onClick,
+  text,
+  variant,
+  href,
+  children,
+  className,
+}) => {
   return variant === 'link' ? (
-    <a href={href} onClick={onClick} className={styles.link}>
-      {text}
+    <a
+      href={href}
+      onClick={onClick}
+      className={classnames(className, styles.link)}
+    >
+      {text || children}
     </a>
   ) : (
     <button
       onClick={onClick}
-      className={classnames(styles.button, styles[`${variant}`])}
+      className={classnames(className, styles.button, styles[`${variant}`])}
     >
-      {text}
+      {text || children}
     </button>
   )
 }
