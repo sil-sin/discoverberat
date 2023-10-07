e34e76bc89e3cf2b8c8196edc50c79fab8033dbf
Silvi Sinanaj <silvi.sinanaj@blacklane.com>
2023-10-07 13:02:54 +0200

    added link for services

--- src/components/sectors/OurServices/OurServices.tsx
+++ src/components/sectors/OurServices/OurServices.tsx
@@ -14,32 +14,42 @@ export const OurServices: FC = () => {
     {
       title: 'Tour Packages',
       icon: <MdTravelExplore size={50} />,
+      link: '/tour-packages',
     },
     {
       title: 'Transport and Transfers',
       icon: <MdEmojiTransportation size={50} />,
+      link: '/transport',
     },
     {
       title: 'Accommodation Booking',
       icon: <MdOutlineLocalHotel size={50} />,
+      link: '/accommodation',
     },
     {
       title: 'Guided Tours and Activities',
       icon: <GiCornerFlag size={50} />,
+      link: '/tours',
     },
   ]
+
   return (
     <div className={styles.servicesContainer}>
       <h2 className={styles.servicesTitle}>Our Services</h2>
       <div className={styles.cardsContainer}>
         {services.map((service) => (
-          <Card
-            iconCard={true}
-            cardIcon={service.icon}
-            className={styles.card}
+          <a
+            className={styles.cardLink}
             key={service.title}
-            title={service.title}
-          />
+            href={service.link}
+          >
+            <Card
+              iconCard={true}
+              cardIcon={service.icon}
+              className={styles.card}
+              title={service.title}
+            />
+          </a>
         ))}
       </div>
     </div>
