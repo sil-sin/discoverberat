c20c03a946217c25251b4e86cf78a79ea207330b
Silvi Sinanaj <silvi.sinanaj@blacklane.com>
2023-10-11 12:11:53 +0200

    commit test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>
         <Navigation />
-        <div className='container'>
-        {children}
-        </div>
+        <div className='container'>{children}</div>
       </body>
     </html>
   )
