00f8a75f17a2d0dc92739dc4482a5700877c211b
Silvi Sinanaj <silvi.sinanaj@blacklane.com>
2023-10-25 00:11:03 +0200

    Update index.tsx

--- src/pages/tours/index.tsx
+++ src/pages/tours/index.tsx
@@ -1,6 +1,7 @@
 import styles from './index.module.css'
 import Tours from '@/components/sectors/Tours/Tours'
-export default function Page() {
+export default function Page({entries}) {
+alert(entries)
   return (
     <div className={styles.container}>
       <Tours />
