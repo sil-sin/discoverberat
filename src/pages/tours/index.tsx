8b3828ec9487503f8ef5a2435fe76faa7b8b0af0
Silvi Sinanaj <silvi.sinanaj@blacklane.com>
2023-10-25 00:33:00 +0200

    Update index.tsx

--- src/pages/tours/index.tsx
+++ src/pages/tours/index.tsx
@@ -1,10 +1,14 @@
 import styles from './index.module.css'
 import Tours from '@/components/sectors/Tours/Tours'
 export default function Page({entries}:{entries:any}) {
-alert(entries)
+!entries && <>No entry</>
+  
+
+  
   return (
     <div className={styles.container}>
       <Tours />
+      <>entries?.[0]?.fields.title</>
     </div>
   )
 }
