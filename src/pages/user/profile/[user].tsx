20849c84fbb6bfb052a9912cc2d5c9bcd095caf8
Silvi Sinanaj <silvi.sinanaj@blacklane.com>
2024-10-25 22:43:10 +0200

    this again

--- src/pages/user/profile/[user].tsx
+++ src/pages/user/profile/[user].tsx
@@ -26,9 +26,7 @@ const ProfilePage: FC = ({ user, savedItems, upcomingBookings }: any) => {
               width={50}
               height={50}
             />
-            <h2 className={styles.title}>
-              {'Welcome, ' + user?.displayName}
-            </h2>
+            <h2 className={styles.title}>{'Welcome, ' + user?.displayName}</h2>
           </div>
           <AdminDashboard bookings={upcomingBookings} user={user} />
         </div>
@@ -47,7 +45,7 @@ const ProfilePage: FC = ({ user, savedItems, upcomingBookings }: any) => {
             height={50}
           />
           <h2 className={styles.title}>
-            {'Welcome, ' + user?.displayName ?? ''}
+            {'Welcome, ' + user?.displayName || 'User'}
           </h2>
         </div>
         <Dashboard
